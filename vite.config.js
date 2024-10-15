2baf15ff0f421e1df0c04180d45f9b170dc9e532
saleeque <saleeque00cr7@gmail.com>
2024-10-15 12:15:04 +0530

    commit after change script list my task

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
- 
     proxy: {
       '/workwise': {
         target: 'https://workwise-backend-zr68.onrender.com',
